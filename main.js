d9af40ab8c37c32e8d4f7344df77c7473d79a4a8
Janice Chui <Janice@dhcp-18-189-44-31.dyn.MIT.EDU>
2018-05-14 13:13:24 -0400

    starting code for homepage

---
+++ main.js
@@ -0,0 +1,39 @@
+// Holds DOM elements that don’t change, to avoid repeatedly querying the DOM
+var dom = {};
+
+// ================================================
+// EVENTS
+// ================================================
+
+// Attaching events on document because then we can do it without waiting for
+// the DOM to be ready (i.e. before DOMContentLoaded fires)
+Util.events(document, {
+	// Final initalization entry point: the Javascript code inside this block
+	// runs at the end of start-up when the DOM is ready
+	"DOMContentLoaded": function() {
+
+		Util.one("#story-popup").style.display = "block";
+		Util.one("#story-popup-close").onclick = function() {
+			Util.one("#story-popup").style.display = "none";
+		}
+
+		window.onclick = function(event) {
+		var popup = Util.one("#story-popup");
+		if (event.target == popup) {
+			popup.style.display = "none";
+		}
+    }
+
+
+	},
+
+	// Keyboard events arrive here
+	"keydown": function(evt) {
+	},
+
+	// Click events arrive here
+	"click": function(evt) {
+		console.log(evt.target);
+
+	}
+});
